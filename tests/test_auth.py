a45c24cea9a6ad17bf480c33dd6795e4685f941d
Juliano Gouveia <juliano.gouveia@olist.com>
2016-08-26 15:33:37 -0300

    First auth tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,24 +1,39 @@
-from quasimodo.auth import AuthConf
-
 import unittest
-import os
+from urllib.parse import urlencode
 
+from quasimodo.auth import AuthConf, AuthURL
+
+
+APP_ID = '91624454549617'
+SECRET_KEY = 'iaw2fxRXjRM5mj4qpJkMX'
+
+auth_conf = AuthConf(APP_ID, SECRET_KEY)
 
 class AuthConfTest(unittest.TestCase):
-
-    def setUp(self):
-        os.environ['QUASIMODO_REDIRECT_URI'] = 'http://example.com'
-        os.environ['QUASIMODO_APP_ID'] = '0001112223444555'
-
-    def test_environment_variables_conf(self):
-        redirect_url = os.environ.get('QUASIMODO_REDIRECT_URI')
-        app_id = os.environ.get('QUASIMODO_APP_ID')
-
-        auth_conf = AuthConf()
-
-        self.assertEqual(auth_conf.redirect_url, redirect_url)
-        self.assertEqual(auth_conf.app_id, app_id)
+    def test_properties(self):
+        assert auth_conf.app_id == APP_ID
+        assert auth_conf.secret_key == SECRET_KEY
 
 
-if __name__ == '__main__':
-    unittest.main()
+class AuthURLTest(unittest.TestCase):
+    def test_return(self):
+        auth_url = AuthURL(auth_conf)
+        params = {
+            'response_type': 'code',
+            'client_id': APP_ID
+        }
+
+        assert repr(auth_url) == 'http://auth.mercadolibre.com/authorization?{}'.format(urlencode(params))
+
+    def test_parameters(self):
+        response_type = 'hex'
+        redirect_uri = 'http://example.com'
+
+        auth_url = AuthURL(auth_conf, redirect_uri='http://example.com', response_type='hex')
+        params = {
+            'response_type': response_type,
+            'redirect_uri': redirect_uri,
+            'client_id': APP_ID
+        }
+
+        assert repr(auth_url) == 'http://auth.mercadolibre.com/authorization?{}'.format(urlencode(params))
